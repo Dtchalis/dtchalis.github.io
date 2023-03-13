cf8006773549138c8ea50c03f47b179a946c6ca7
Lynval Wynter <lwynter@live.ca>
2023-03-13 19:14:26 -0400

    updated project files

---
+++ dtchalis-portfolio/src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+    <h1>
+        <img src="/img/poll_ninja_logo.svg" alt="Poll Ninja Logo">
+    </h1>
+</header>
+
+<style>
+    header{
+        background: #f7f7ff;
+        padding: 20px;
+    }
+    h1{
+        margin: 0;
+        text-align: center;
+    }
+    img{
+        width: 180px;
+    }
+</style>
